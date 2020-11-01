d8049ab8d6ec6dd7212992fbfcf0421417558b43
tayor99 <72174183+tayor99@users.noreply.github.com>
2020-11-01 04:02:43 -0800

    Add files via upload

---
+++ salary.js
@@ -0,0 +1,5 @@
+function salary(hoursWorked, overTime){
+    console.log((1000 * hoursWorked) + (200 * overTime))
+};
+
+salary(150, 50)//where hours worked will be inputed for the overall result of salary at the end of the month
